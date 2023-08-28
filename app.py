13e6f5cfe582e21ad27ff1e5e503dc7c0149ea56
Guillermo García Rojas C <garciarojas@protonmail.com>
2023-08-27 21:25:23 -0600

    New file
    Added new file

---
+++ app.py
@@ -0,0 +1,40 @@
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, LLMPredictor, ServiceContext
+from langchain import OpenAI
+import openai
+import gradio as gr
+import os
+import sys
+
+os.environ['OPENAI_API_KEY'] = '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' # noqa
+
+
+def construct_index(directory_path):
+    num_outputs = 512
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
+
+    docs = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTVectorStoreIndex.from_documents(docs, service_context=service_context)
+
+    index.storage_context.persist('index.json')
+
+    return index
+
+index = construct_index("docs")
+
+def chatbot(input_text):
+    query_engine = index.as_query_engine()
+    response = query_engine.query(input_text)
+#    index = GPTVectorStoreIndex.load_from_disk('index.json')
+#    response = index.query(input_text, response_mode="compact")
+    return response.response
+
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.Textbox(lines=7, label="Enter your text:"),
+                     outputs="text",
+                     title="SoloGPT AI Chatbot")
+
+iface.launch(share=True)
